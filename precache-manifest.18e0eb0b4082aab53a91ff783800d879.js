984757e5e6fc52ab7463398d098ecbd98e2cc3f7
Rasheed <a.rasheedalabi@gmail.com>
2020-01-17 21:56:08 +0100

    gh pages commit

---
+++ precache-manifest.18e0eb0b4082aab53a91ff783800d879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8592933b0405e9c1bf276158840218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b5a5abdf92374190860",
+    "url": "/static/css/main.107f5974.chunk.css"
+  },
+  {
+    "revision": "248a6c3ae1ea85ceb5d8",
+    "url": "/static/js/2.46ae75e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.46ae75e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b5a5abdf92374190860",
+    "url": "/static/js/main.bb503929.chunk.js"
+  },
+  {
+    "revision": "df4a41a5d587ce67c87b",
+    "url": "/static/js/runtime-main.207ee5cd.js"
+  }
+]);
